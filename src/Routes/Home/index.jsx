300380ba61258ae23ef264dbef333707a57c3134
Nico Goux <nicolasgoux2000@gmail.com>
2023-01-13 12:39:21 -0300

    finalizada la vista de home

--- src/Routes/Home/index.jsx
+++ src/Routes/Home/index.jsx
@@ -9,7 +9,14 @@ function Home() {
 
 	return (
 		<section className='Home'>
-			<input type='text' className='input search-input' />
+			<div className='search-input-container'>
+				<input placeholder='Search product' type='text' className='input search-input' />
+				<img
+					className='search-icon'
+					src='/src/assets/icons/icon_search.svg'
+					alt='search-icon'
+				/>
+			</div>
 			<p className='title-center'>
 				Order: <span className='text-info'>Most Relevant</span>
 			</p>
